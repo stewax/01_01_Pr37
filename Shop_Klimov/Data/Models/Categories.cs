e769391c769a66c68c341e1db3f34f6dd012d12a
STEPS0N <stepan.klimoff@mail.ru>
2026-05-05 20:38:04 +0500

    Разработанные модели данных. Осенников

---
+++ Shop_Klimov/Data/Models/Categories.cs
@@ -0,0 +1,10 @@
+﻿namespace Shop_Klimov.Data.Models
+{
+    public class Categories
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Items> Items { get; set; }
+    }
+}
---
+++ Shop_Klimov/Data/Models/Items.cs
@@ -0,0 +1,12 @@
+﻿namespace Shop_Klimov.Data.Models
+{
+    public class Items
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Img { get; set; }
+        public int Price { get; set; }
+        public Categories Category { get; set; }
+    }
+}
