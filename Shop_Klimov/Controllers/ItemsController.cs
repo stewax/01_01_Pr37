9f9cb4ebde0f33d1a5f1076c8463ea5887e69d65
STEPS0N <stepan.klimoff@mail.ru>
2026-05-05 21:02:42 +0500

    Разработанный контроллер предметов. Осенников

---
+++ Shop_Klimov/Controllers/ItemsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop_Klimov.Data.Interfaces;
+
+namespace Shop_Klimov.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private IItems IAllItems;
+        private ICategorys IAllCategorys;
+
+        public ItemsController(IItems IAllItems, ICategorys IAllCategorys)
+        {
+            this.IAllItems = IAllItems;
+            this.IAllCategorys = IAllCategorys;
+        }
+
+        public ViewResult List()
+        {
+            ViewBag.Title = "Страница с предметами";
+
+            var cars = IAllItems.AllItems;
+            return View(cars);
+        }
+    }
+}
