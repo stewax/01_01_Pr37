7662d24a263ee87ecc46ab730a5ae5c1cffde925
STEPS0N <stepan.klimoff@mail.ru>
2026-05-07 08:49:58 +0500

    Подключение стилей к страницам. Осенников

--- Shop_Klimov/Program.cs
+++ Shop_Klimov/Program.cs
@@ -1,12 +1,17 @@
+using Shop_Klimov.Data.Interfaces;
+using Shop_Klimov.Data.Mocks;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMvc(option => option.EnableEndpointRouting = false);
+
+builder.Services.AddTransient<ICategorys, MockCaregorys>();
+builder.Services.AddTransient<IItems, MockItems>();
+
 var app = builder.Build();
 
 app.UseDeveloperExceptionPage();
 app.UseStatusCodePages();
 app.UseStaticFiles();
 app.UseMvcWithDefaultRoute();
-
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
