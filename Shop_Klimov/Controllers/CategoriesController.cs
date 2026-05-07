0cff2f2f6a384a2d2927c8421b176fd4fdeea04f
STEPS0N <stepan.klimoff@mail.ru>
2026-05-07 10:17:47 +0500

    Разработанное практическое занятие на оценку хорошо и отлично. Осенников

---
+++ Shop_Klimov/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop_Klimov.Data.Interfaces;
+
+namespace Shop_Klimov.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private ICategorys IAllCategorys;
+        private IItems IAllItems;
+
+        public CategoriesController(ICategorys IAllCategorys, IItems IAllItems)
+        {
+            this.IAllCategorys = IAllCategorys;
+            this.IAllItems = IAllItems;
+        }
+
+        public ViewResult Categories()
+        {
+            ViewBag.Title = "Страница с категориями";
+
+            var categories = IAllCategorys.AllCategories;
+            return View(categories);
+        }
+    }
+}
