adcf8b0b11b1b8328c1b292a20da5147f2757be1
STEPS0N <stepan.klimoff@mail.ru>
2026-05-13 18:06:40 +0500

    Метод добавления предмета. Осенников

--- Shop_Klimov/Controllers/ItemsController.cs
+++ Shop_Klimov/Controllers/ItemsController.cs
@@ -7,14 +7,17 @@ namespace Shop_Klimov.Controllers
 {
     public class ItemsController : Controller
     {
+        private readonly IHostEnvironment hostingEnvironment;
+
         private IItems IAllItems;
         private ICategorys IAllCategorys;
         VMItems VMItems = new VMItems();
 
-        public ItemsController(IItems IAllItems, ICategorys IAllCategorys)
+        public ItemsController(IItems IAllItems, ICategorys IAllCategorys, IHostEnvironment environment)
         {
             this.IAllItems = IAllItems;
             this.IAllCategorys = IAllCategorys;
+            this.hostingEnvironment = environment;
         }
 
         public ViewResult List(int id = 0, string sort = "")
@@ -43,6 +46,37 @@ namespace Shop_Klimov.Controllers
             IEnumerable<Categories> Categories = IAllCategorys.AllCategories;
 
             return View(Categories);
+        }
+
+        /// <summary>
+        /// Метод добавления предмета
+        /// </summary>
+        /// <param name="name">Наименование предмета</param>
+        /// <param name="description">Описание предмета</param>
+        /// <param name="files">Изображение</param>
+        /// <param name="price">Цена</param>
+        /// <param name="idCategory">Код категории</param>
+        /// <returns></returns>
+        [HttpPost]
+        public RedirectResult Add(string name, string description, IFormFile files, float price, int idCategory)
+        {
+            if (files != null)
+            {
+                var uploads = Path.Combine(hostingEnvironment.ContentRootPath, "img");
+                var filePath = Path.Combine(uploads, files.FileName);
+                files.CopyTo(new FileStream(filePath, FileMode.Create));
+            }
+
+            Items newItems = new Items();
+            newItems.Name = name;
+            newItems.Description = description;
+            newItems.Img = files.FileName;
+            newItems.Price = Convert.ToInt32(price);
+            newItems.Category = new Categories() { Id = idCategory };
+
+            int id = IAllItems.Add(newItems);
+
+            return Redirect("/Items/Update?id=" + id);
         }
     }
 }
